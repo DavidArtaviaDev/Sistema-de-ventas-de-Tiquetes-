6d51ded8ef79bf4e3af5321668ba7ce4f5ff5c76
DavidArtaviaDev <artaviadavid23r@gmail.com>
2025-09-13 13:58:59 -0600

    Implementacion de la opcion 1 del menu,Funcion Inicio de sesion y cargardatos pero solo de clientes

--- Main.py
+++ Main.py
@@ -1,84 +1,15 @@
 from Ticket import Ticket
 from GestorTicket import GestorTicket
+from Tiquetera import Tiquetera
 
 def menu():
-    gestor = GestorTicket()
+    tiquetera = Tiquetera()
 
-    while True:
-        print("\n=== MENU GESTION DE TICKETS ===")
-        print("1. Crear ticket")
-        print("2. Listar tickets")
-        print("3. Buscar ticket por ID")
-        print("4. Actualizar ticket")
-        print("5. Cancelar ticket")
-        print("6. Eliminar ticket")
-        print("7. Deshacer ultima operacion")
-        print("0. Salir")
+    tiquetera.cargarDatos()
+    
+    tiquetera.mostraMenuPrincipal()
 
-        opcion = input("Seleccione una opcion: ")
 
-        if opcion == "1":
-            id_evento = input("ID del evento: ")
-            id_cliente = input("ID del cliente: ")
-            sector = input("Sector (Graderia/Gramilla/VIP): ")
-            precio = input("Precio: ")
-
-            try:
-                ticket = Ticket("", id_evento, id_cliente, sector, precio)
-                if gestor.crear_ticket(ticket):
-                    print(f"Ticket creado con ID {ticket.id_ticket}")
-            except ValueError as e:
-                print(f"Error: {e}")
-
-        elif opcion == "2":
-            gestor.listar_tickets()
-
-        elif opcion == "3":
-            id_ticket = input("Ingrese el ID del ticket: ")
-            ticket = gestor.obtener_ticket(id_ticket)
-            if ticket:
-                print(ticket)
-            else:
-                print("Ticket no encontrado.")
-
-        elif opcion == "4":
-            id_ticket = input("Ingrese el ID del ticket a actualizar: ")
-            campo = input("Campo a actualizar (sector/precio/estado): ")
-            nuevo_valor = input(f"Nuevo valor para {campo}: ")
-
-            # Conversión de precio a float si corresponde
-            if campo == "precio":
-                try:
-                    nuevo_valor = float(nuevo_valor)
-                except ValueError:
-                    print("Error, el precio debe ser numerico.")
-                    continue
-
-            if gestor.actualizar_ticket(id_ticket, **{campo: nuevo_valor}):
-                print( "Ticket actualizado.")
-            else:
-                print("No se pudo actualizar el ticket.")
-
-        elif opcion == "5":
-            id_ticket = input("Ingrese el ID del ticket a cancelar: ")
-            gestor.cancelar_ticket(id_ticket)
-
-        elif opcion == "6":
-            id_ticket = input("Ingrese el ID del ticket a eliminar: ")
-            if gestor.eliminar_ticket(id_ticket):
-                print("Ticket eliminado.")
-            else:
-                print("Ticket no encontrado.")
-
-        elif opcion == "7":
-            gestor.deshacer()
-
-        elif opcion == "0":
-            print("Fuera de sistema")
-            break
-
-        else:
-            print("Opcion invalida.")
 
 
 if __name__ == "__main__":
--- Tiquetera.py
+++ Tiquetera.py
@@ -1,218 +1,24 @@
 # Es recomendable importar las clases que se usarán para type hinting (opcional pero buena práctica)
 from typing import List, Optional
 from Auth import Auth
+from CSVManager import CSVManager
 from Cliente import Cliente
 from Evento import Evento
 
 class Tiquetera:
- 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Parte de David 
+    # Parte de David
 
     def __init__(self):
         self.clientes = []
-        self.auth = Auth(self.clientes)
+      
+        self.cliente_actual = None  # Cliente autenticado actualmente el que esta utilizando la aplicacion en momento
+        self.auth = None    # Objeto Auth para manejar autenticaciones se carga en la funcion cargarDatos
+
+
 
     def mostraMenuPrincipal(self):
-        print("DEBUG: Entrando al método mostraMenuPrincipal")  # Mensaje de depuración
+   
         while True:
             print("\n==============================")
             print("  Bienvenido al sistema de venta de tiquetes")
@@ -223,13 +29,83 @@ class Tiquetera:
             opcion = input("\nDigite la opción que desea realizar: ").strip()
 
             if opcion == "1":
-                print("\n[Iniciar sesión seleccionado]")
-                return 1
+                self.inicioDeSesion()
+                #mostrar el menu del cliente autenticado
+                #if self.cliente_actual is not None:
+                   # self.mostrarMenuCliente()
+                  
             elif opcion == "2":
                 print("\n[Registrar cliente seleccionado]")
-                return 2
+                
             elif opcion == "3":
-                print("\n¡Gracias por usar el sistema! Hasta luego.")
-                return 3
+                break
             else:
                 print("\nOpción no válida. Por favor, intente de nuevo.")
+ 
+    def inicioDeSesion(self):
+
+        print  ("\nDigite 1 para cliente o 2 para admin")
+        opcion = input("\nDigite la opción que desea realizar: ").strip()
+        if opcion == "1":
+
+         print("\n--- Inicio de Sesión ---")
+         id_cliente = input("Ingrese su ID de cliente: ").strip()
+         clave = input("Ingrese su clave: ").strip()
+        
+         if self.auth is None:
+          print("El sistema de autenticacion no se ha inicializado.")
+          print("Por favor, contacte al administrador.")
+          return 
+    
+         resultado, self.cliente_actual = self.auth.autenticar(id_cliente, clave)
+
+        
+         if resultado == "EXITO" and self.cliente_actual is not None:
+            
+            print(f"\n¡Bienvenido, {self.cliente_actual.nombre}!") 
+            
+
+         elif resultado == "CLAVE_INCORRECTA":
+
+            print("\nClave incorrecta. Por favor, intente de nuevo.")
+
+         elif resultado == "CLIENTE_NO_ENCONTRADO":
+
+            print("\nID de cliente no encontrado. Por favor, registrese primero.")
+            
+         else:
+            print("\nError desconocido durante la autenticación.")
+
+        if opcion == "2":
+         print("\n--- Inicio de Sesión Admin ---")
+         id_admin = input("Ingrese su ID de admin: ").strip()
+         clave_admin = input("Ingrese su clave: ").strip()
+        
+         if id_admin == "admin" and clave_admin == "admin123":
+            print("\n¡Bienvenido, Admin!")
+            # Aquí puedes agregar el menú o funcionalidades específicas para el admin
+         else:
+            print("\nCredenciales de admin incorrectas. Por favor, intente de nuevo.")
+ 
+
+    def cargarDatos(self):
+      
+        # 1. Usamos  CSVManager para llenar la lista de clientes.
+        self.clientes = CSVManager.cargar_clientes("clientes.csv")
+        
+        # 2. Creamos la instancia de Auth con la lista de clientes ya cargada.
+        self.auth = Auth(self.clientes)
+        
+        # para saber cuantos clientes se cargaron
+        print(f"¡Listo! Se cargaron {len(self.clientes)} clientes.")
+        
+
+    def menuAdmin(self):
+        print("\n--- Menú Admin ---")
+        print("1 - Ver todos los clientes")
+        print("2 - Buscar cliente por ID")
+        print("3 - Agregar evento")
+        print("4 - Cerrar sesión")
+        opcion = input("\nSeleccione una opción: ").strip()
+
+     
